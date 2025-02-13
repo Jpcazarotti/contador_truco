e9a97c0d96c7bbf937c0a7ec9e3e48cd4eb77377
João Pedro Cazarotti Batisata <joaopedrocazarottijp@gmail.com>
2025-02-13 15:29:26 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:contador_truco/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Contador de Truco",
+    home: HomePage(),
+  ));
+}
